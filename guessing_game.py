322f8a006e2eb60d7f9654f7b04699f83aeda467
Indrajith <67006308+indar12@users.noreply.github.com>
2021-06-27 15:34:23 +0530

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,14 +1,23 @@
-import random
-generate = random.randint(1,10)
+from random import randint
+
+start = int(input("Start bound: "))
+
+end = int(input("End bound: "))
+
+guess_answer = randint(start, end)
+
 while True:
     try:
-        guess = int(input("Guess the number 1 to 10: "))
-        if 0 < guess < 11:
-            if guess == generate:
-                print("You are correct!!!")
+        guess = int(input('Guess the number between {0} and {1}: '.format(start,end)))
+        if start <= guess <=end:
+            if guess == guess_answer:
+                print("You Won!!!")
                 break
+            else:
+                 print("You lost!!! Try again")
+                 continue
         else:
-            print("Enter a number between 1 to 10")
+            print("Enter your guess between {0} and {1}".format(start,end))
     except ValueError:
         print("Please enter a number")
         continue
